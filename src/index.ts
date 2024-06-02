86b584f9e5ab82083de411870e1e3c9ddf0d08e2
Vathanak Norng <sovanvathanaknorng@gmail.com>
2024-06-02 20:20:03 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,29 @@ import { createCategory, deleteCategory, getAllCategories, getCategoryById, upda
 import { createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from './controllers/product-controller';
 const app = express();
 
-// parse application/json
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('hello world!')
 })
 
-
 app.route('/categories')
   .get(getAllCategories)
   .post(createCategory)
+
 app.route('/categories/:categoryId')
   .get(getCategoryById)
   .patch(updateCategory)
   .delete(deleteCategory)
 
-
 app.route('/products')
   .get(getAllProducts)
   .post(createProduct)
+
 app.route('/products/:productId')
   .get(getProductById)
   .patch(updateProduct)
   .delete(deleteProduct)
-
 
 app.listen(3000, () => {
   console.log(`Listen on port 3000`)
